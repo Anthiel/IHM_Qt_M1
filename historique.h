c019fc24f6b1b2d848e8c119d3af97d4868dacc5
Ptilulu83 <lucas.moragues@wanadoo.fr>
2019-02-06 11:29:54 +0100

    début annuler rétablir

--- historique.h
+++ historique.h
@@ -4,8 +4,27 @@
 
 class historique
 {
+
+public:
+    bool can_undo;
+    bool can_redo;
+
+private:
+    int actual_element;
+    int first_element;
+    int last_element;
+    int tabSize;
+
 public:
     historique();
+    void on_actionUndo_triggered();
+    void on_actionRedo_triggered();
+    void on_newModification_added();
+
+private:
+    void set_actual_element();
 };
+
+
 
 #endif // HISTORIQUE_H
